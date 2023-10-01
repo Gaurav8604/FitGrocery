c48351d610de701821cc841a30418de93df26ffc
Gaurav-8604 <gauravpatil8604@gmail.com>
2023-10-01 20:01:58 +0530

    tenth-commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import Home from './PAGES/HomePage/Home'
+import Home from './PAGES/Homepage/Home'
 import './App.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
